ee67388835485e66932153dbc7f0c85bbc0923b6
DK <101485484+dong-hei@users.noreply.github.com>
2024-09-30 18:08:38 +0900

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -5,13 +5,13 @@ import { getAnalytics } from 'firebase/analytics';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyDFtqzJY1ccG0EFdD7SI6hNySoQYMdivxA',
-  authDomain: 'reactnative-todo-670de.firebaseapp.com',
-  projectId: 'reactnative-todo-670de',
-  storageBucket: 'reactnative-todo-670de.appspot.com',
-  messagingSenderId: '1010066918118',
-  appId: '1:1010066918118:web:384c7d8116fdab35428927',
-  measurementId: 'G-L0FZNXY0X6',
+  apiKey: 'apiKey',
+  authDomain: 'authDomain',
+  projectId: 'projectId',
+  storageBucket: 'storageBucket',
+  messagingSenderId: 'messagingSenderId',
+  appId: 'appId',
+  measurementId: 'measurementId',
 };
 
 // Initialize Firebase
